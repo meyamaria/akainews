5fd9c74fc3d530aed2cdae2b5b0b14fab08b97b7
meyamaria <133187874+meyamaria@users.noreply.github.com>
2025-03-23 19:10:17 +0530

    Update tts_generator.py

--- backend/tts_generator.py
+++ backend/tts_generator.py
@@ -1,7 +1,28 @@
 
-from TTS.api import TTS
+from gtts import gTTS
+from googletrans import Translator
+from flask import jsonify, send_from_directory
+import os
+
+AUDIO_DIR = "audios"
 
 def generate_tts(text):
-    tts = TTS("tts_models/multilingual/multi-dataset/your_model").to("cpu")
-    tts.tts_to_file(text=text, file_path="output.mp3", lang="hi")
-    return "output.mp3"
+    translator = Translator()
+
+    translated = translator.translate(text, dest='hi')
+    print("Translated text:", translated.text)
+
+    tts = gTTS(text=translated.text, lang='hi')
+    filename = "output.mp3"
+    filepath = os.path.join( AUDIO_DIR, filename )
+    os.makedirs(AUDIO_DIR, exist_ok=True)
+    tts.save(filepath)
+    return jsonify({"audio": f"audio/{filename}"})
+
+def returnDir(filename):
+    return send_from_directory(
+        AUDIO_DIR,
+        filename,
+        mimetype="audio/mpeg",
+        download_name=filename
+    )
