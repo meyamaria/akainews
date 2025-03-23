c4d0067afea9fcfecd11688ed58b8cb0adc5cc2e
meyamaria <133187874+meyamaria@users.noreply.github.com>
2025-03-23 19:09:22 +0530

    Update news_scraper.py

--- backend/news_scraper.py
+++ backend/news_scraper.py
@@ -5,12 +5,12 @@ def fetch_news_articles(company_name):
     search_url = f"https://www.bing.com/news/search?q={company_name}&form=NWRFSH"
     response = requests.get(search_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    
     articles = []
-    for item in soup.find_all('a', href=True)[:10]:
+    for item in soup.find_all('a', href=True)[40:50]:
         title = item.get_text()
         link = item['href']
-        if title and link.startswith("http"):
+        if title and (link.startswith("http") or link.startswith("http")):
             articles.append({"title": title, "link": link})
 
     return articles
